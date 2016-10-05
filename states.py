e65b22b05043799bd492e42ed1d39a23fe59bb38
bopes <clcblee@gmail.com>
2016-10-04 17:25:58 -0700

    Add states list and abbrev method

---
+++ states.py
@@ -0,0 +1,67 @@
+us_states = [
+  "Alabama",
+  "Alaska",
+  "Arizona",
+  "Arkansas",
+  "California",
+  "Colorado",
+  "Connecticut",
+  "Delaware",
+  "Florida",
+  "Georgia",
+  "Hawaii",
+  "Idaho",
+  "Illinois",
+  "Indiana",
+  "Iowa",
+  "Kansas",
+  "Kentucky",
+  "Louisiana",
+  "Maine",
+  "Maryland",
+  "Massachusetts",
+  "Michigan",
+  "Minnesota",
+  "Mississippi",
+  "Missouri",
+  "Montana",
+  "Nebraska",
+  "Nevada",
+  "New Hampshire",
+  "New Jersey",
+  "New Mexico",
+  "New York",
+  "North Carolina",
+  "North Dakota",
+  "Ohio",
+  "Oklahoma",
+  "Oregon",
+  "Pennsylvania",
+  "Rhode Island",
+  "South Carolina",
+  "South Dakota",
+  "Tennessee",
+  "Texas",
+  "Utah",
+  "Vermont",
+  "Virginia",
+  "Washington",
+  "West Virginia",
+  "Wisconsin",
+  "Wyoming"
+]
+
+us_state_abbrvs = {
+  'Florida': "Fla.",
+  "Texas": "Tex.",
+  "Wisconsin": "Wis."
+}
+
+def abbrev_state(str):
+  abbrev_str = str
+  for state, abbrev in us_state_abbrvs.items():
+    if state + "." in abbrev_str:
+      abbrev_str = abbrev_str.replace(state + ".", abbrev)
+    elif state in abbrev_str:
+      abbrev_str = abbrev_str.replace(state, abbrev)
+  return abbrev_str
