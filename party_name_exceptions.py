3313c6230217f31debc9bd8dd50b8cba9da633c3
bopes <clcblee@gmail.com>
2016-10-04 17:24:01 -0700

    Create party name exceptions file

---
+++ party_name_exceptions.py
@@ -0,0 +1,14 @@
+capitalized_acronyms = [
+  "LLC",
+  "LLC,"
+]
+
+allowed_abbrvs = {
+  "CENTER": "Ctr.",
+  "CENTER,": "Ctr.,",
+  "DEPARTMENT": "Dep't"
+}
+
+allowed_lowers = {
+  'OF': 'of',
+}
