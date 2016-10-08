8cd1ee8a5a2f2f828a74568dd33b3024bb30cf80
bopes <clcblee@gmail.com>
2016-10-08 14:50:04 -0700

    Refactor and comment out find_court

--- string_parser.py
+++ string_parser.py
@@ -1,6 +1,7 @@
-import states
-import party_name_exceptions
+import courts
 import general_reference
+import party_name_exceptions
+import states
 
 def format_party_name(str):
   formatted_name = ""
@@ -24,27 +25,64 @@ def format_party_name(str):
   return formatted_name
 
 
-def find_court(str1, str2):
-  if str1 == "Supreme Court of the United States":
-    court = ""
-  elif str1 == "United States Court of Appeals,":
-    for word in str2.split():
-      if word in general_reference.order_numerals.keys():
-        court = "%s Cir. " % general_reference.order_numerals[word]
-  elif "United States District Court" in str1:
-    split_court = str1.split(", ")
-    court = states.abbrev_state(split_court[1]) + " "
-  elif "Appeals" in str1:
-    court = "%s App. " % states.find_state(str1)
-  else:
-    court = states.find_state(str1) + " "
-  return court
+
+
+# def supreme_crt(str1, str2):
+#   return ""
+
+# def us_crt_appeals(str1, str2):
+#   for word in str2.split():
+#     if word in general_reference.order_numerals.keys():
+#       return "%s Cir. " % general_reference.order_numerals[word]
+
+# def us_dist_crt(str1, str2):
+#   split_court = str1.split(", ")
+#   return states.abbrev_state(split_court[1]) + " "
+
+# def appeals(str1, str2):
+#   return "%s App. " % states.find_state(str1)
+
+# courts = {
+#   "Supreme Court of the United States": supreme_crt,
+#   "United States Court of Appeals": us_crt_appeals,
+#   "United States District Court": us_dist_crt,
+#   "Appeals": appeals
+# }
+
+# sorted_courts = [
+#   "Supreme Court of the United States",
+#   "United States Court of Appeals",
+#   "United States District Court",
+#   "Appeals"
+# ]
+
+# def find_court(str1, str2):
+#   for court in sorted_courts:
+#     if court in str1:
+#       return courts[court](str1,str2)
+#   return states.find_state(str1) + " "
+#   # court = str
+#   # if str1 == "Supreme Court of the United States":
+#   #   court = ""
+#   # elif str1 == "United States Court of Appeals,":
+#   #   for word in str2.split():
+#   #     if word in general_reference.order_numerals.keys():
+#   #       court = "%s Cir. " % general_reference.order_numerals[word]
+#   # elif "United States District Court" in str1:
+#   #   split_court = str1.split(", ")
+#   #   court = states.abbrev_state(split_court[1]) + " "
+#   # elif "Appeals" in str1:
+#   #   court = "%s App. " % states.find_state(str1)
+#   # else:
+#   #   court = states.find_state(str1) + " "
+#   # return court
+
 
 
 def convert_citation(raw_citation, pages):
   citation_list = raw_citation.split('\r\n')
   case_id = citation_list[0]
-  court = find_court(citation_list[1], citation_list[2])
+  court = courts.find_court(citation_list[1], citation_list[2])
   v_location = citation_list.index('v.')
   raw_party_1 = citation_list[v_location - 1]
   party_1 = format_party_name(raw_party_1)
