89db86bc3a91a9bb1d0910e192bc48a17ddc7f8c
bopes <clcblee@gmail.com>
2016-10-08 21:42:54 -0700

    Add prefix handling

--- parties.py
+++ parties.py
@@ -18,28 +18,6 @@ allowed_lowers = {
   'OF': 'of',
 }
 
-
-# def format_party_name(str):
-  # party_name = ""
-  # org_words = str.split()
-  # for word in org_words:
-  #   if word == word.upper() and word not in general_reference.initials:
-  #     if word in allowed_abbrvs.keys():
-  #       word = allowed_abbrvs[word]
-  #     elif word in allowed_lowers.keys():
-  #       word = allowed_lowers[word]
-  #     elif word not in capitalized_acronyms:
-  #       word = word.capitalize()
-  #     if word in states.us_state_abbrvs.keys():
-  #       word = states.us_state_abbrvs[word]
-  #     party_name += word + " "
-  # # This is breakable, only works for "LLC," right now
-  # if party_name[-2] == ',' and party_name[-5:-2] != "LLC":
-  #   party_name = party_name[:-2]
-  # else:
-  #   party_name = party_name[:-1]
-  # return party_name
-
 converters = [
   allowed_abbrvs,
   allowed_lowers
@@ -53,8 +31,25 @@ trailing_punc = [
   'LLC'
 ]
 
+lowercase_prefixes = [
+  'Mc',
+  'Mac'
+]
+
+def has_lowercase_prefix(word):
+  for prefix in lowercase_prefixes:
+    if word.startswith(prefix):
+      word = word.replace(prefix,"")
+      if word == word.upper():
+        return True
+  return False
+
 def part_of_party_name(word):
-  return word == word.upper() and word not in general_reference.initials
+  if word == word.upper() and word not in general_reference.initials:
+    return True
+  elif has_lowercase_prefix(word):
+    return True
+  return False
 
 def converted(word):
   for converter in converters:
@@ -68,6 +63,23 @@ def nonconverted(word):
       return word
   return False
 
+def correct_capitalization(word):
+  output = ""
+  if converted(word):
+    return converted(word)
+  elif nonconverted(word):
+    return word
+  elif states.find_state(word.capitalize()):
+    return states.find_state(word.capitalize())
+  elif has_lowercase_prefix(word):
+    for prefix in lowercase_prefixes:
+      if word.startswith(prefix):
+        rest_of_word = word.replace(prefix,"")
+        return prefix + rest_of_word.capitalize()
+  else:
+    return word.capitalize()
+  output += " "
+
 def has_trailing_punc(party_name):
   if party_name[-2] in string.punctuation:
     for punc_word in trailing_punc:
@@ -75,38 +87,24 @@ def has_trailing_punc(party_name):
         return True
   return False
 
-def set_trailing_punc(party_name):
+def correct_trailing_punc(party_name):
   if has_trailing_punc(party_name):
     return party_name[:-1]
+  elif party_name[-2] in string.punctuation:
+    return party_name[:-2]
   else:
-   return party_name[:-2]
+    return party_name[:-1]
 
-def format_party_name(party):
-  party_list = party.split()
-  party_name = ""
-
-  # Look at each word in party name
+def get_party_name(party):
+  party_list, party_name = party.split(), ""
   for word in party_list:
-    # Check if word should be kept (is mostly caps)
     if part_of_party_name(word):
-      if converted(word):
-        party_name += converted(word)
-      elif nonconverted(word):
-        party_name += word
-      elif states.find_state(word.capitalize()):
-        party_name += states.find_state(word.capitalize())
-      else:
-        party_name += word.capitalize()
-      party_name += " "
-
-  # Should the party have a trailing comma? If so, keep it
-  party_name = set_trailing_punc(party_name)
-
+      party_name += correct_capitalization(word) + " "
+  party_name = correct_trailing_punc(party_name)
   return party_name
-
 
 def find_parties(citation_list):
   v = citation_list.index('v.')
   p1, p2 = citation_list[v-1], citation_list[v+1]
-  p1, p2 = format_party_name(p1), format_party_name(p2)
+  p1, p2 = get_party_name(p1), get_party_name(p2)
   return p1, p2
