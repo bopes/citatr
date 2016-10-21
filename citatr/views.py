819b43fe5c51370ca11f72b20bfa16ba18892782
bopes <clcblee@gmail.com>
2016-10-21 16:57:42 -0700

    Add sign up route

--- citatr/views.py
+++ citatr/views.py
@@ -19,12 +19,12 @@ def login():
   if request.method == "POST":
     db = get_db()
     # Encode the user provided password for hash comparison
-    given_pw = bytes(request.form['password'], 'utf-8')
+    encode_pw = bytes(request.form['password'], 'utf-8')
     # Find the given username in databse (SQLi safe)
     cmd = "SELECT * FROM users WHERE username=?"
     user = db.execute(cmd,(request.form['username'],)).fetchone()
     # Check that the user exists and password validates
-    if user and bcrypt.hashpw(given_pw, user[2]) == user[2]:
+    if user and bcrypt.hashpw(encode_pw, user[2]) == user[2]:
       session['logged_in'] = True
       return redirect(url_for('index'))
     # Invalid credential handling
@@ -39,7 +39,7 @@ def login():
 @app.route("/logout")
 def logout():
   session['logged_in'] = False
-  flash('Successfully logged out.')
+  flash('Account logged out.')
   return redirect(url_for('login'))
 
 
@@ -48,6 +48,34 @@ def index():
   if not session.get('logged_in'):
     return redirect(url_for('login'))
   return render_template('index.html')
+
+
+@app.route('/signup', methods=["GET","POST"])
+def signup():
+  if request.method == "GET":
+    if not session.get('logged_in'):
+      return render_template('signup.html')
+    return redirect(url_for('index'))
+  else:
+    # Check all fields are entered
+    if any(entry == "" for entry in request.form):
+      error = 'All values must be filled.'
+      return render_template('signup')
+    # Confirm password
+    if request.form['password'] != request.form['password_cnf']:
+      error = 'Password and password confirmation must match.'
+      return render_template('signup')
+    # Add user to table
+    db = get_db()
+    encode_pw = bytes(request.form['password'], 'utf-8')
+    hashed_pw = bcrypt.hashpw(encode_pw, bcrypt.gensalt())
+    cmd = "INSERT INTO users (username, password) VALUES (?,?)"
+    db.execute(cmd,(request.form['username'], hashed_pw))
+    db.commit()
+    flash('Account created.')
+    return redirect(url_for('login'))
+
+
 
 
 @app.route("/convert", methods=['POST'])
