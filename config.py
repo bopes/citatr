90bf4f45baf4273165c1d8e83e57fe02a99b5d5a
bopes <clcblee@gmail.com>
2016-10-20 22:28:16 -0700

    Update config

--- config.py
+++ config.py
@@ -1,15 +1,7 @@
-import os
-from citatr import app
 
 
-root_dir = os.path.abspath(os.path.dirname(__file__))
-
-# Debug key
-SECRET_KEY='2BBCFA59DC64CB1353FFC2148E634'
-
-# Admin login
-USERNAME='bigep'
+DATABASE=os.path.join(app.root_path, 'citatr.db'),
+DEBUG=True,
+SECRET_KEY='2BBCFA59DC64CB1353FFC2148E634',
+USERNAME='bigep',
 PASSWORD='yankeehotelfoxtrot'
-
-# DB Config
-DATABASE=os.path.join(root_dir, 'db/citatr.db'),
