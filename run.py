11d0be2f3bf6d75df9167f8aebc2f058413db440
bopes <clcblee@gmail.com>
2016-10-20 22:26:34 -0700

    Initialize db if doesn't exist

--- run.py
+++ run.py
@@ -1,5 +1,9 @@
 import os
-from citatr import app
+from citatr import app, init_db
+
+db_loc = os.path.join(app.root_path, 'citatr.db')
+if not os.path.isfile(db_loc):
+  init_db()
 
 port = int(os.environ.get('PORT', 5000))
 app.run(host='0.0.0.0', port=port)
