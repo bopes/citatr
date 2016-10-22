bd526758c1c8e3e5d6789dc45eb46f2af203aca3
bopes <clcblee@gmail.com>
2016-10-22 15:07:25 -0700

    Move db functions to db file

--- citatr/__init__.py
+++ citatr/__init__.py
@@ -13,36 +13,10 @@ with ctx:
 
 app.config.update(dict(
     DATABASE=os.path.join(app.root_path, 'citatr.db'),
-    DEBUG=True,
+    # DEBUG=True,
     SECRET_KEY='2BBCFA59DC64CB1353FFC2148E634',
     USERNAME='bigep',
     PASSWORD='yankeehotelfoxtrot'
 ))
-
-# Create database connection
-def connect_db():
-  rv = sqlite3.connect(app.config['DATABASE'])
-  rv.row_factory = sqlite3.Row
-  return rv
-
-# Open database connection
-def get_db():
-  if not hasattr(g, 'sqlite_db'):
-    g.sqlite_db = connect_db()
-  return g.sqlite_db
-
-# Initialize database
-def init_db():
-  db = get_db()
-  with app.open_resource('db/schema.sql', mode='r') as f:
-    db.cursor().executescript(f.read())
-  db.commit()
-
-
-# Close database connection
-@app.teardown_appcontext
-def close_db(error):
-  if hasattr(g, 'sqlite_db'):
-    g.sqlite_db.close()
 
 from citatr import views
