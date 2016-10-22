d20cc8f23433a416a8a772e3f6fc818a12b9633a
bopes <clcblee@gmail.com>
2016-10-22 15:08:06 -0700

    Add db reset

--- run.py
+++ run.py
@@ -1,9 +1,6 @@
 import os
-from citatr import app, init_db
+from citatr import app, db
 
-db_loc = os.path.join(app.root_path, 'citatr.db')
-if not os.path.isfile(db_loc):
-  init_db()
-
+db.reset_db()
 port = int(os.environ.get('PORT', 5000))
 app.run(host='0.0.0.0', port=port)
