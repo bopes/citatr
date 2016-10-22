b192e3b9d921f0fd6ecbecb08e72ef191b18749e
bopes <clcblee@gmail.com>
2016-10-22 15:06:17 -0700

    Remove extra files

--- citatr/db/db_drop.py
+++
@@ -1,8 +0,0 @@
-import os
-
-current_dir = os.path.dirname(__file__)
-db_dir = os.path.join(current_dir, "..")
-db_file = os.path.join(db_dir, 'citatr.db')
-
-os.remove(db_file)
-print("Existing database deleted...")
